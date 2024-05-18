6cec4d3555c05a81f22780b63f08d63502765c92
Oliver Madine <30090176+OliverMadine@users.noreply.github.com>
2024-05-18 23:14:13 +0100

    Add multi cradle

---
+++ src/Compiler/Gigaparsec.hs
@@ -0,0 +1,9 @@
+{-# LANGUAGE TypeFamilies #-}
+
+module Compiler.Gigaparsec where
+
+import Combinators ( Combinator )
+
+compile :: Combinator a
+compile = undefined
+
--- src/compiler/GigaparsecCompiler.hs
+++
@@ -1,8 +0,0 @@
-{-# LANGUAGE TypeFamilies #-}
-
-module Compiler.GigaparsecCompiler where
-
-import Combinators
-
--- compile :: Combinator a -> Parser a
--- compile Pure = pure ()
